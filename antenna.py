e2e744f9f6ab8f56e15a18ac5e17a4afabb5e39e
Niels Moseley <niels@nowhere.com>
2021-06-15 01:38:02 +0200

    WIP

--- antenna.py
+++ antenna.py
@@ -7,15 +7,17 @@ import matplotlib.cm as cm
 
 # we abuse the complex data type as a 2D vector to simplify things
 # waveLength in metres
+# txphase is in degrees
 #
 # This formula is valid at distances greater than twice the wavelength
 #
-def calcSignalVectorFromAntenna(antennaPos : np.csingle, myPos : np.csingle, waveLength : float) -> np.csingle : 
+def calcSignalVectorFromAntenna(antennaPos : np.csingle, myPos : np.csingle, waveLength : float, txphase : float) -> np.csingle : 
 
     distance = np.absolute(myPos - antennaPos)  # calculates the Euclidian norm i.e. the distance
 
     amplitude = 1.0/(distance*waveLength)
-    wavelengths = distance / waveLength
+    #amplitude = 1
+    wavelengths = distance / waveLength + txphase / 360.
 
     signal = amplitude * (np.cos(-2*np.pi*wavelengths) + 1j*np.sin(-2*np.pi*wavelengths))
 
@@ -26,7 +28,9 @@ def displayPattern():
     ant1Location = -10.  # (-20, 0)
     ant2Location =  10.  # ( 20, 0)
 
-    viewWidth    = 8000.  # 800 metre view on both sides
+    txphase = -45.
+
+    viewWidth    = 800.  # 800 metre view on both sides
 
     n = 256 
     x = np.linspace(-viewWidth, viewWidth, n) 
@@ -38,7 +42,7 @@ def displayPattern():
     for xx in x:
         idx2 = 0
         for yy in y:
-            amplitude = np.absolute(calcSignalVectorFromAntenna(ant1Location, xx + 1.j*yy, 40) + calcSignalVectorFromAntenna(ant2Location, xx + 1.j*yy, 40))
+            amplitude = np.absolute(calcSignalVectorFromAntenna(ant1Location, xx + 1.j*yy, 40, 0) + calcSignalVectorFromAntenna(ant2Location, xx + 1.j*yy, 40, txphase))
             amp[idx1, idx2] = 20.0*np.log10(amplitude)
             idx2 = idx2 + 1
         idx1 = idx1 + 1
@@ -51,9 +55,9 @@ def displayPattern():
     amp2 = np.zeros((n,1))
     idx1 = 0
     for xx in x:
-        amplitude1 = np.absolute(calcSignalVectorFromAntenna(ant1Location, 1j*xx, 40) + calcSignalVectorFromAntenna(ant2Location, 1j*xx, 40))
+        amplitude1 = np.absolute(calcSignalVectorFromAntenna(ant1Location, 1j*xx, 40, 0) + calcSignalVectorFromAntenna(ant2Location, 1j*xx, 40, txphase))
         amp1[idx1] = 20.0*np.log10(amplitude1)
-        amplitude2 = np.absolute(calcSignalVectorFromAntenna(ant1Location, xx, 40) + calcSignalVectorFromAntenna(ant2Location, xx, 40))
+        amplitude2 = np.absolute(calcSignalVectorFromAntenna(ant1Location, xx, 40, 0) + calcSignalVectorFromAntenna(ant2Location, xx, 40, txphase))
         amp2[idx1] = 20.0*np.log10(amplitude2)
         idx1 = idx1 + 1
     
@@ -68,10 +72,10 @@ def printSignal(sig : np.csingle):
     print("  angle    : ", np.angle(sig)/np.pi*180.0)
 
 print("Signal at a distance of 20m from an antenna TXing on 40m")
-printSignal(calcSignalVectorFromAntenna(0,20,40))
+printSignal(calcSignalVectorFromAntenna(0,20,40,0))
 
 print("Combined signal from two 40m TX antennas at (-20,0) and (20,0) observed at (0,0)")
-printSignal(calcSignalVectorFromAntenna(-20,0,40) + calcSignalVectorFromAntenna(20,0,40))
+printSignal(calcSignalVectorFromAntenna(-20,0,40,0) + calcSignalVectorFromAntenna(20,0,40,0))
 
 displayPattern()
 
